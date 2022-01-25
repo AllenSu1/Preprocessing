e00ee1b2925e57602a8186edf7297374eb019b31
AllenSu1 <74522190+AllenSu1@users.noreply.github.com>
2022-01-25 21:24:30 +0800

    update

---
+++ pick_same_files.py
@@ -0,0 +1,24 @@
+# 複製資料夾下檔案對應的xml到另個資料夾
+import os, random, shutil
+
+
+def copyFile(fileDir, xmlDir, tarDir):
+    '''
+    fileDir是獲取檔名的路徑
+    xmlDir是fileDir所對應檔案的路徑
+    tarDir複製檔案的路徑
+    '''
+
+    pathDir = os.listdir(fileDir)
+    print(pathDir)
+
+    for name in pathDir:
+        name = os.path.splitext(name)[0] + '.xml'
+        shutil.copyfile(xmlDir + name, tarDir + name)
+
+
+if __name__ == '__main__':
+    fileDir = "D:/Dataset/Allen_thesis/vannamei/vannamei_v1/image/"
+    xmlDir = "D:/Dataset/Allen_thesis/vannamei/vannamei_monkey/pic_black_all_label/"
+    tarDir = 'D:/Dataset/Allen_thesis/vannamei/vannamei_v1/xml/'
+    copyFile(fileDir, xmlDir, tarDir)
--- randomly_pick_files.py
+++ randomly_pick_files.py
@@ -1,15 +1,19 @@
+# 複製資料夾下檔案到另個資料夾
 import os, random, shutil
+
 
 def copyFile(fileDir):
 
-	pathDir = os.listdir(fileDir)
+    pathDir = os.listdir(fileDir)
 
-	sample = random.sample(pathDir, 2000)
-	print (sample)
-	
-	for name in sample:
-		shutil.copyfile(fileDir+name, tarDir+name)
+    sample = random.sample(pathDir, 10000)
+    print(sample)
+
+    for name in sample:
+        shutil.copyfile(fileDir + name, tarDir + name)
+
+
 if __name__ == '__main__':
-	fileDir = "D:/Dataset/dogs_and_cats/train/cat/"
-	tarDir = 'D:/Dataset/dogs_and_cats/z_test/train/cats/'
-	copyFile(fileDir)
+    fileDir = "D:/Dataset/Allen_thesis/vannamei/vannamei_v1/image/"
+    tarDir = 'D:/Dataset/Allen_thesis/vannamei/vannamei_v1/xml/'
+    copyFile(fileDir)
