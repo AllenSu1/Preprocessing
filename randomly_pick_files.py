8a30f8a2e65af58b4cca2aa131f112731e2263fb
AllenSu1 <74522190+AllenSu1@users.noreply.github.com>
2022-02-25 14:13:35 +0800

    update

--- randomly_pick_files.py
+++ randomly_pick_files.py
@@ -1,19 +1,28 @@
 # 複製資料夾下檔案到另個資料夾
 import os, random, shutil
+from sqlalchemy import true
 
 
-def copyFile(fileDir):
+def copyFile(fileDir, Copy, num):
 
-    pathDir = os.listdir(fileDir)
+    if Copy == True:
 
-    sample = random.sample(pathDir, 100)
-    print(sample)
+        pathDir = os.listdir(fileDir)
+        sample = random.sample(pathDir, num)
+        print(sample)
 
-    for name in sample:
-        shutil.copyfile(fileDir + name, tarDir + name)
+        for name in sample:
+            shutil.copyfile(fileDir + name, tarDir + name)
+        print('複製成功！')
+
+    else:
+        print('未複製成功！')
 
 
 if __name__ == '__main__':
-    fileDir = "D:/Dataset/Allen_thesis/vannamei/vannamei_v1/image/"
-    tarDir = 'E:/test/'
-    copyFile(fileDir)
+    fileDir = "C:/Users/Allen/Desktop/2/"
+    tarDir = 'C:/Users/Allen/Desktop/1/'
+    numFile = 3  # 欲隨機複製檔案的個數
+    readyCopy = False  # 防手誤 True表示開始複製
+
+    copyFile(fileDir, readyCopy, numFile)
