a541672f90a6ee2b8ec638e6183044a739049a95
AllenSu1 <74522190+AllenSu1@users.noreply.github.com>
2022-03-30 16:57:18 +0800

    update

--- pick_same_files.py
+++ pick_same_files.py
@@ -1,5 +1,6 @@
 # 複製資料夾下檔案對應的xml到另個資料夾
 import os, random, shutil
+from pathlib import Path
 
 
 def copyFile(fileDir, xmlDir, tarDir):
@@ -19,6 +20,28 @@ def copyFile(fileDir, xmlDir, tarDir):
 
 if __name__ == '__main__':
     fileDir = "D:/Dataset/VOC2007/JPEGImages/"
-    xmlDir = "D:/Dataset/Allen_thesis/vannamei/vannamei_monkey/pic_black_all_label/"
+    xmlDir = "D:/Dataset/Annotations/"
     tarDir = 'D:/Dataset/VOC2007/Annotations/'
+
+    # 執行 複製資料夾下檔案對應的xml到另個資料夾
     copyFile(fileDir, xmlDir, tarDir)
+
+    jpgFile = os.listdir(fileDir)
+    xmlFile = os.listdir(tarDir)
+
+    # 以下為檢驗部分 確認複製的檔案是否對應
+    error = 0
+    for i in range(len(jpgFile)):
+        jpgFile[i] = os.path.splitext(jpgFile[i])[0]
+        xmlFile[i] = os.path.splitext(xmlFile[i])[0]
+
+        # print(jpgFile[i])
+        # print(xmlFile[i])
+
+        if jpgFile[i] == xmlFile[i]:
+            pass
+        else:
+            error += 1
+
+    if error == 0:
+        print('執行成功！')
--- randomly_pick_move_files.py
+++ randomly_pick_move_files.py
@@ -3,18 +3,30 @@
 ## pic是字典，存放每個圖片要移到的子資料夾名
 
 import os, random, shutil
+
+
 def moveFile(fileDir):
-        pathDir = os.listdir(fileDir)    # 取圖片的原始路徑
-        filenumber=len(pathDir)
-        rate=0.1    # 抽取圖片比例
-        picknumber=int(filenumber*rate) # 按照rate比例從文件夾中取一定數量圖片
-        sample = random.sample(pathDir, picknumber)  # 隨機選取picknumber數量的樣本圖片
-        print (sample)
-        for name in sample:
-                shutil.move(fileDir+name, tarDir+name)
-        return
+
+    rate_true = True  # 是否按比例或個數挑選檔案 (True 按比例挑選 False 按個數挑選)
+    rate = 0.1  # 抽取圖片比例
+    picknumber = 16000  # 挑選圖片張數
+
+    pathDir = os.listdir(fileDir)  # 取圖片的原始路徑
+    filenumber = len(pathDir)
+
+    if rate_true:
+        picknumber = int(filenumber * rate)  # 按照rate比例從文件夾中取一定數量圖片
+    else:
+        picknumber = picknumber
+
+    sample = random.sample(pathDir, picknumber)  # 隨機選取picknumber數量的樣本圖片
+    print(sample)
+    for name in sample:
+        shutil.move(fileDir + name, tarDir + name)
+    return
+
 
 if __name__ == '__main__':
-	fileDir = "D:/Dataset/defect_detection/choose_2/train/5/"    # 原圖片文件夾路徑
-	tarDir = r'D:\\Dataset\\defect_detection\\choose_2\\val\\5\\'    # 移動到新的文件夾路徑
-	moveFile(fileDir)
+    fileDir = "D:/Dataset/Train/VOC2007/JPEGImages/"  # 原圖片文件夾路徑
+    tarDir = 'D:/Dataset/Test/VOC2007/JPEGImages/'  # 移動到新的文件夾路徑
+    moveFile(fileDir)
